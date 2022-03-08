731937a46e64cfa1e0badd2763348c83fd1ca367
이정환 <king199777@gmail.com>
2022-03-08 16:57:46 +0900

    [Fix] : Fix runtime error

--- components/notion/NotionImage.tsx
+++ components/notion/NotionImage.tsx
@@ -16,13 +16,6 @@ type Props = notion_types.ImageItem;
 // COMPONENT
 
 const NotionImage = (props: Props) => {
-  var date = new Date();
-  const iso = date.toISOString(); //"2011-12-19T15:28:46.493Z"
-
-  console.log(
-    iso.replaceAll("-", "").replaceAll(":", "").split(".")[0].concat("Z")
-  );
-
   let imageUrl = "";
   if (props.image.type === "external") {
     imageUrl = props.image.external.url;
