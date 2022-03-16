ea577122fa3acff9d69b7960306a9e413009523b
이정환 <king199777@gmail.com>
2022-03-16 14:57:02 +0900

    [Refactor] : react-notion-x로 변경 및 guide페이지 추가 (#5)

---
+++ pages/guide/[page_id].tsx
@@ -0,0 +1,52 @@
+import BlockRenderer from "components/Common/BlockRenderer";
+import DetailPageSkeleton from "components/Common/DetailPageSkeleton";
+import PaddingContainer from "components/Common/PaddingContainer";
+import { getPageBlocks } from "lib/server/get-page-blocks";
+import moment from "moment-timezone";
+import { useRouter } from "next/router";
+import { ExtendedRecordMap } from "notion-types";
+import { getBlockTitle, getPageBreadcrumbs } from "notion-utils";
+
+interface Props {
+  pageId: string;
+  recordMap: ExtendedRecordMap;
+  lastFetch: string;
+}
+const Guide = (props: Props) => {
+  const { pageId, recordMap, lastFetch } = props;
+
+  const router = useRouter();
+  if (router.isFallback) {
+    return <DetailPageSkeleton />;
+  }
+  const keys = Object.keys(recordMap?.block || {});
+  const block = recordMap?.block?.[keys[0]]?.value;
+  const title = getBlockTitle(block, recordMap);
+  return (
+    <PaddingContainer>
+      <h2>{title}</h2>
+      <BlockRenderer pageId={pageId} blocks={recordMap} lastFetch={lastFetch} />
+    </PaddingContainer>
+  );
+};
+export default Guide;
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+export const getStaticProps = async (ctx) => {
+  const { page_id } = ctx.params;
+  const lastFetch = moment().tz("Asia/Seoul").toString();
+  const recordMap = await getPageBlocks(page_id);
+  return {
+    props: {
+      pageId: page_id,
+      recordMap,
+      lastFetch,
+    },
+    revalidate: 1,
+  };
+};
---
+++ pages/guide/index.tsx
@@ -0,0 +1,43 @@
+import BlockRenderer from "components/Common/BlockRenderer";
+import PaddingContainer from "components/Common/PaddingContainer";
+import { getPageBlocks } from "lib/server/get-page-blocks";
+import moment from "moment-timezone";
+import { ExtendedRecordMap } from "notion-types";
+interface Props {
+  pageId: string;
+  recordMap: ExtendedRecordMap;
+  lastFetch: string;
+}
+const Guide = (props: Props) => {
+  const { pageId, recordMap, lastFetch } = props;
+  return (
+    <PaddingContainer>
+      <BlockRenderer pageId={pageId} blocks={recordMap} lastFetch={lastFetch} />
+    </PaddingContainer>
+  );
+};
+export default Guide;
+
+export const getStaticProps = async () => {
+  const guidePageId = "3b660121653f4daaa403ef5c2b72a4ac";
+  const lastFetch = moment().tz("Asia/Seoul").toString();
+  const recordMap = (await getPageBlocks(guidePageId)) as ExtendedRecordMap;
+
+  const blockIds = Object.keys(recordMap.block);
+  blockIds.forEach((blockId) => {
+    const block = recordMap.block[blockId];
+    if (block.value?.type === "page") {
+      const source = "guide/" + block.value.id;
+      recordMap.block[blockId].value.id = source;
+    }
+  });
+
+  return {
+    props: {
+      pageId: guidePageId,
+      recordMap,
+      lastFetch,
+    },
+    revalidate: 1,
+  };
+};
