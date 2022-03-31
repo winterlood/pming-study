4313558613941ea860fb76c21424770c9e7d6d04
이정환 <king199777@gmail.com>
2022-03-31 15:32:50 +0900

    버전 변경 없음 - 스터디 지원시 슬랙 알림 추가 (#21)
    * [Refactor] : react-notion-x로 변경 및 guide페이지 추가
    
    * [Fix] : 스터디 멘토 가이드 링크 수정
    
    * [Feat] : sitemap 추가 및 구글 서치콘솔 연동
    
    * [Feat] : Naver 웹 마스터 추가
    
    * [Refactor] : 릴리즈 데이터베이스 설정 변경 (#9)
    
    * Update README.md
    
    * [Style] : 스타일 수정
    
    * [Text] : Footer 주관사 명 수정
    
    * [Fix] : study recruit 페이지 마감일 수 수정
    
    * Refactor ver2 (#17)
    
    * [Style] : 스터디, 포스트 아이템 스타일 변경
    
    * [Fix] : ItemGrid 레거시 컴포넌트 import 제거
    
    * [Fix] : 사용하지 않는 import 제거
    
    * [Feat] : 유데미 요청에 따라 CTA버튼 추가 및 배너 수정
    
    * [Fix] : recruit페이지 태그 변경
    
    * [Feat] : Revalidate 조절 및 UTM활성화
    
    * [FEAT] : slack study apply webhook 추가

---
+++ lib/server/apply-webhook.ts
@@ -0,0 +1,98 @@
+import { api_types } from "@types";
+import axios from "axios";
+
+const WEBHOOK_URL =
+  "https://hooks.slack.com/services/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";
+
+interface WebhookProperty extends api_types.StudyApplyRequestBody {
+  isSuccess: boolean;
+  apply_date: string;
+  study_page_url: string;
+}
+
+export const applyWebhook = (param: WebhookProperty) => {
+  axios.post(WEBHOOK_URL, {
+    attachments: [
+      {
+        pretext: param.isSuccess
+          ? `✅STUDY APPLY✅\n\n\n새로운 스터디 지원이 있습니다.\n\n현재 시간 : ${param.apply_date}\n아래 링크를 클릭하여 해당 스터디를 확인할 수 있습니다`
+          : `🔥ERROR OCCURED🔥\n\n\n스터디 지원 오류가 발생하였습니다\n\n현재 시간 : ${param.apply_date}\n아래 링크를 클릭하여 해당 스터디를 확인할 수 있습니다`,
+        title: `${param.target_study_id} 스터디`,
+        title_link: param.study_page_url,
+        color: param.isSuccess ? "blue" : "danger",
+        fields: [
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "지원 일자(apply_date)",
+            value: param.apply_date,
+            short: true,
+          },
+          {
+            title: "지원 스터디 ID(target_study_id)",
+            value: param.target_study_id,
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "지원자 이름(applicant_name)",
+            value: param.applicant_name,
+            short: true,
+          },
+          {
+            title: "지원자 카카오 ID(applicant_kakao_id)",
+            value: param.applicant_kakao_id,
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "지원자 GitHub 주소(applicant_github_url)",
+            value: param.applicant_github_url,
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "",
+            value: "",
+            short: true,
+          },
+          {
+            title: "지원 동기(applicant_reason)",
+            value: param.applicant_reason,
+            short: false,
+          },
+        ],
+      },
+    ],
+  });
+};
